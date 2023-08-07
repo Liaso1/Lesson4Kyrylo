b431a7bc4ad3ffcd9f30db9dd2ddc77cc589d7aa
Liaso1 <liasol97@gmail.com>
2023-08-06 21:38:44 -0400

    third message

--- Src/ProgramLesson4.java
+++ Src/ProgramLesson4.java
@@ -2,5 +2,6 @@ public class ProgramLesson4 {
     public static void main(String[] args) {
         System.out.println("Hello git");
         System.out.println("Hello git from Kyrylo");
+        System.out.println("Hello git from our group");
     }
 }
